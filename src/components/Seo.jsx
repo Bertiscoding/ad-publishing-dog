9d7ad1f0b52ef42e0fc1c00fc0cf085733c157ee
Helene <40670839+Bertiscoding@users.noreply.github.com>
2023-11-12 21:33:05 -0500

    new seo file

---
+++ src/components/Seo.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { useSiteMetadata } from "../hooks/use-site-metadata"
+
+const Seo = ({ title, description, image, url, children }) => {
+  const { title: defaultTitle, description: defaultDescription, image: defaultImage, url: siteUrl } = useSiteMetadata()
+
+  const seo = {
+    title: title || defaultTitle,
+    description: description || defaultDescription,
+    image: image || `${siteUrl}/${defaultImage}`,
+    url: url || siteUrl
+  }
+
+  return (
+    <>
+      <title>{seo.title}</title>
+      <meta name="description"  content={seo.description} />
+      <meta name="image"  content={seo.image} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:url" content={seo.url} />
+      { children }
+    </>
+  )
+}
+
+export default { Seo };

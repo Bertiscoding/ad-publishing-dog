d4aabeb56ac3ed54695a7e519c85b2b5c0589427
Helene <40670839+Bertiscoding@users.noreply.github.com>
2023-11-18 16:51:17 +0000

    Update Seo.jsx

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -20,6 +20,7 @@ const Seo = ({ title, description, image, url, children }) => {
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
       <meta property="og:url" content={seo.url} />
+      <meta name="google-site-verification" content="-HCSRS0gcE81KKVnGBgstPQtJuiEEI0GTALmnkXViUU" />
       { children }
     </>
   )
